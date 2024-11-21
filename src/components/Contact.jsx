bd6dd961afa1d6edf8c38b9f6dda4bc881bb304b
Abdulrahman92 <chahdiadbulrahman@gmail.com>
2024-11-21 12:33:41 +0100

    update site

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section id="contact" className="section">
       <div className="container">
         <h2>Contactez-moi</h2>
-        <p>Email : <a href="mailto:chahdiabdulrahman@example.com">chahdiabdulrahman@example.com</a></p>
+        <p>Email : <a href="mailto:chahdiabdulrahman@gmail.com">chahdiabdulrahman@gmail.com</a></p>
         <p>LinkedIn : <a href="https://www.linkedin.com/in/abdulrahman-chahdi-23379520b/" target="_blank" rel="noopener noreferrer">Mon Profil LinkedIn</a></p>
       </div>
     </section>
